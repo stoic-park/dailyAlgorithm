c9e3d891bf0fb0d86c256cb658dccbb7f67f69b5
stoic-park <demige79@gmail.com>
2020-06-04 18:08:31 +0900

    :fire: day23 큰 수 만들기
    - greedy 탐색
    - 세부적인 조건 처리하는 부분의 테스트 케이스가 까다로운 문제
    - 한번 더 보는 것을 추천

--- day23.js
+++ day23.js
@@ -71,3 +71,46 @@ function solution(number, k) {
 // 다만, 이 문제에서 나를 힘들게 했던 것은
 // 문자열에서 특정 인덱스 값을 지우는 방법과
 // 내가 만든 for loof에 내가 빠져버렸다는 것...?
+
+function solution(number, k) {
+  var stack = []; // b : 최종 글자들이 저장될 스택 (숫자가 큰!)
+  for (var i = 0; i < number.length; i++) {
+    // 모든 숫자 비교
+    var now = number[i]; // 현재 인덱스 숫자. 처음에는 그냥 push
+
+    // 무조건 push 하고 다음 for 문에서 이전인덱스 숫자와 나랑 비교해서 현재인덱스가 이전인덱스보다 크면 pop하고 넣어줌. 제거하는 숫자인 (k)를 1 감소
+    // 제거할 숫자를 다 채웠으면 그냥 넣음
+    while (k > 0 && stack[stack.length - 1] < now) {
+      stack.pop();
+      k--;
+    }
+    stack.push(now);
+  }
+  // k가 0일 경우 스택은 그대로,
+  // but k가 남아있으면 뒤에서부터 제거해준다. (ex. 1010,2 -> 11)
+  // ? 이걸 해주지 않으면 테스트 12를 통과하지 못한다
+  stack.splice(stack.length - k, k);
+  var answer = stack.join("");
+
+  return answer;
+}
+
+// 출처 : https://velog.io/@kimtaeeeny/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%81%B0-%EC%88%98-%EB%A7%8C%EB%93%A4%EA%B8%B0-javascript
+
+function solution(number, k) {
+  const answer = [];
+  let head = 0;
+  let del = k;
+
+  answer.push(number[head++]);
+  while (answer.length < number.length - k || head < number.length) {
+    if (del && answer[answer.length - 1] < number[head]) {
+      answer.pop();
+      del--;
+      continue;
+    }
+    answer.push(number[head++]);
+  }
+
+  return answer.slice(0, number.length - k).join("");
+}
