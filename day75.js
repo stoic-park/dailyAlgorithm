4417985882cb65be91e4daa003338225d397490f
stoic-park <demige79@gmail.com>
2020-08-17 09:50:33 +0900

    :fire: Fix: day75.js complete
    - 문제를 잘 이해하자.
    - 반복문에서 코드가 실행되는 순서를
    - 콘솔을 찍어 확인해보자

--- day75.js
+++ day75.js
@@ -48,3 +48,70 @@ var increasingTriplet = function (nums) {
 
 // 문제를 잘못 이해한듯..?
 // 다시 풀어보자
+
+// 문제를 다시 잘 보면
+// 연속적으로 3개가 되는 조건이 있다
+// : such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+
+// [5,1,5,5,2,5,4]
+// 근데 이게 왜 true 가 나오느거야?
+// 1,2,4 를 선택해서 true가 되는건가?
+// 그럼 타겟을 고정해야 하는건가?
+// first, second가 존재하고, nums[i]가 second보다 크다면 true?
+
+var increasingTriplet = function (nums) {
+  // 0. 기본 예외 조건
+  if (nums.length < 3) return false;
+  let first = nums[0];
+  let second = Infinity;
+  for (let i in nums) {
+    if (nums[i] < first) first = nums[i];
+    if (nums[i] > first && nums[i] < second) second = nums[i];
+    if (nums[i] > second) return true;
+  }
+  return false;
+};
+
+// 후기
+// 문제가 이해가 안간다잉
+// first를 지정함에 있어서 nums[0]은 통과가 되고, Math.min을 사용했을 경우는 안되는 이유 ???
+// 문제 자체가 말하는 경우가 뭐지?
+// 연속적으로 3개가 증가하는 경우가 아니라
+// 0 <= first < second < target 인 경우가 있으면 true를 리턴하라는 것 같다
+// Infinity를 사용하는 이유?
+
+// if (n > second) return true;
+// if (n > first) second = n;
+// else first = n;
+// 의 경우는 통과하고
+
+// if (nums[i] < first) first = nums[i];
+// if (nums[i] > first && nums[i] < second) second = nums[i];
+// else return true;
+// 의 경우는 통과하지 않는 이유?
+
+// console.log( first, second ) 를 찍어봤을 때
+// 1의 경우에는
+// undefined undefined
+// 1 undefined
+// 1 2
+
+// 2의 경우에는
+// undefined undefined
+
+// 결론 : target을 지정하는 순서의 차이
+
+// reference
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/781/discuss/79010/JavaScript-solution-emphasizing-logical-clarity
+
+// solution
+var increasingTriplet = function (nums) {
+  let first, second; //first and second smallest
+
+  for (let n of nums) {
+    if (n > second) return true;
+    if (n > first) second = n;
+    else first = n;
+  }
+  return false;
+};
